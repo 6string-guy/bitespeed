1d9a34e8ed749ad1973b1150d4007495d35ff23b
6string-guy <sanskarjaiswal010@gmail.com>
2025-05-26 22:37:35 +0530

    setting up server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import connection from "./db.js";
+import express from 'express'
+const app = express();
+// connection.query("SELECT NOW()", (err, results) => {
+//   if (err) throw err;
+//   console.log("Current time:", results[0]);
+// });
 
-connection.query("SELECT NOW()", (err, results) => {
-  if (err) throw err;
-  console.log("Current time:", results[0]);
+const PORT = process.env.SERVER_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
