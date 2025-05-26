1d7218d3dc98fb0beedc9eb249347d65d0d8f877
6string-guy <sanskarjaiswal010@gmail.com>
2025-05-26 23:32:47 +0530

    implementing identify route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import connection from "./db.js";
 import express from 'express'
+import cors from 'cors'
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 // connection.query("SELECT NOW()", (err, results) => {
 //   if (err) throw err;
 //   console.log("Current time:", results[0]);
@@ -10,3 +14,29 @@ const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/contacts", (req, res) => {
+  connection.query("SELECT * FROM contacts", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Database error" });
+    }
+    res.json(results);
+  });
+});
+
+app.post("/identify", (req, res) => {
+  const { email, phoneNumber } = req.body;
+  connection.query(
+    "SELECT * FROM contacts WHERE email = ? OR phoneNumber = ?",
+    [email, phoneNumber],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Database error" });
+      }
+      res.json(results);
+    }
+  );
+});
+  
+  
