895c8c3562c6b5e91f3081b71e80f716492b7ac7
6string-guy <sanskarjaiswal010@gmail.com>
2025-05-27 15:19:19 +0530

    pool connection setup

--- db.js
+++ db.js
@@ -1,26 +1,30 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise"; 
 import dotenv from "dotenv";
 import fs from "fs";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.PORT), 
+  port: Number(process.env.PORT),
   user: "avnadmin",
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
     ca: fs.readFileSync(process.env.DB_SSL_CA || "ca.pem"),
   },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("MySQL connection error:", err);
-    return;
-  }
-  console.log("Connected to Aiven MySQL!");
-});
 
-export default connection;
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log("Connected to Aiven MySQL");
+    connection.release();
+  })
+  .catch((err) => console.error("Connection error:", err));
+
+export default pool;
